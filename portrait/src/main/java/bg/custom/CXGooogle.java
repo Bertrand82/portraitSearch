aa5165da840efb7b8bd63f89f03241a860b6590d
Bertrand GUIRAL <bertrand.guiral@gmail.com>
2019-11-14 20:05:04 +0100

    bg

---
+++ portrait/src/main/java/bg/custom/CXGooogle.java
@@ -0,0 +1,13 @@
+package bg.custom;
+/**
+ * 
+ * creer un cx via :
+ * via https://cse.google.com/all
+ *
+ */
+public class CXGooogle {
+
+	// 
+	public static String CX = "xxxxxxxxxxxxxxxxxx";
+
+}
